4811337a0910668a50ae1261e22bad2c00b9c63b
gksl9 <gksl9@112.166.133.118>
2022-01-11 22:05:42 +0900

    commit2

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -18,7 +18,7 @@ public class Gugudan {
 		//3단
 //		System.out.println("3단");
 //		System.out.println(3 * 1);
-//		System.out.println(3 * 2);
+//		System.out.println(3 * 2); 
 //		System.out.println(3 * 3);
 //		System.out.println(3 * 4);
 //		System.out.println(3 * 5);
